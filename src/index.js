f1754bab760c835f05f3c0ef319da7ae0e450237
Kim <72332310+kimnle@users.noreply.github.com>
2024-09-03 19:30:34 +1000

    feat: project init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let defaultEnv = {
+    PORT: 3030,
+    SOME_DATABASE_URL: "blah blah blah",
+    SECURITY_TOKEN: "gkljadglkjglkasdj",
+    SECRET_API_KEY: "3847190jkgdhla1895741fdjalk",
+    JWT_SECRET_KEY: "some special jwt salt word or phrase"
+}
+
+console.log(defaultEnv);
